df5e9f4f8e5eb7531d600018e04a436b1bd06877
silvertside <153336271+ssilvertt@users.noreply.github.com>
2024-08-30 19:15:14 +0200

    deposit

--- src/components/DepositCard.tsx
+++ src/components/DepositCard.tsx
@@ -1,13 +1,14 @@
-
 import CartSVG from '@svg/cart.svg?react';
 import BinanceSVG from '@svg/binance.svg?react';
 import HrSVG from '@svg/hr.svg?react';
 import InvestSVG from '@svg/invest.svg?react';
 import CollectSVG from '@svg/collect.svg?react';
+import CalcSVG from '@svg/calc.svg?react';
+import { ProgressBar } from './ui/ProgressBar.tsx';
 export function DepositCard() {
     return (
         <div
-            className="w-full h-[457px] mx-auto border-t-[2.5px] border-[#282a35] rounded-[25px] relative overflow-hidden p-6"
+            className="w-full mx-auto border-t-[2.5px] border-[#282a35] rounded-[25px] relative overflow-hidden p-6"
             style={{
                 backgroundColor: '#1c2133',
                 backgroundImage: 'radial-gradient(circle at bottom right, var(--color-white), transparent 70%)',
@@ -37,18 +38,43 @@ export function DepositCard() {
             </div>
             <HrSVG className="mt-2" />
             <div className="flex justify-between mt-3">
-                <button
-                    className="rounded-[10.5px] bg-gradient-to-b from-[#4204ff] to-[#2e9cfe] px-4 py-2 border-t border-[#3D4AFF] flex gap-x-1">
+                <button className="rounded-[10.5px] bg-gradient-to-b from-[#4204ff] to-[#2e9cfe] px-4 py-2 border-t border-[#3D4AFF] flex gap-x-1">
                     <InvestSVG />
                     <p className="font-proxima font-bold text-[13.75px] leading-[110%] tracking-[-2%]">Инвестировать</p>
                 </button>
-                <button
-                    className="rounded-[10.5px] bg-gradient-to-b from-[#181921] to-[#181A22] px-4 py-2 border-t border-[#0F1014] flex gap-x-1">
+                <button className="rounded-[10.5px] bg-gradient-to-b from-[#181921] to-[#181A22] px-4 py-2 border-t border-[#0F1014] flex gap-x-1">
                     <CollectSVG />
                     <p className="font-proxima font-bold text-[13.75px] leading-[110%] tracking-[-2%]">Инвестировать</p>
                 </button>
             </div>
             <HrSVG className="mt-2" />
+            <div>
+                <div className="flex mt-4 items-center gap-x-2">
+                    <CalcSVG />
+                    <p className="font-proxima text-[21px] leading-[26px] tracking-normal">Калькулятор дохода</p>
+                </div>
+                <div className="flex justify-between mt-1">
+                    <div className="flex flex-col">
+                        <p className="text-[11.6px] font-bold font-proxima leading-4 text-[#F7C853]">Binance</p>
+                        <p className="text-[7.73px] leading-[9px] opacity-50">Текущий тариф</p>
+                        <p className="text-[17.23px] leading-[21px] font-bold mt-1">0₽</p>
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-[11.6px] font-bold font-proxima leading-4 text-[#6DDAB6]">Tether</p>
+                        <p className="text-[7.73px] leading-[9px] opacity-50">Следующий</p>
+                        <p className="text-[17.23px] leading-[21px] font-bold mt-1">0₽</p>
+                    </div>
+                </div>
+                <div className='mt-3'>
+                    <ProgressBar progress="50" />
+                </div>
+                <div className='flex flex-col mt-3 gap-y-2'>
+                    <p className="text-[17.23px] leading-[21px] font-bold mt-1">Личный депозит: 0₽ </p>
+                    <p className="text-[17.23px] leading-[21px] font-bold mt-1">До следующего тарифа: 9222₽ </p>
+                </div>
+            
+            </div>
+        
         </div>
     );
 }
