2e90fd260bd4e88466d86682e882419f2737e484
silvertside <153336271+ssilvertt@users.noreply.github.com>
2024-08-30 19:39:53 +0200

    Earned

---
+++ src/components/ui/IncomeStats.tsx
@@ -0,0 +1,22 @@
+export function IncomeStats(){
+	const rowTexts = [
+		"На депозитах",
+		"Собрано прибыли",
+		"По партнерской программе",
+		"Инвестированно за 7 дней",
+		"Прибыль за 7 дней"
+	];
+	return(
+		<div className="max-w-full rounded-[14px] backdrop-blur-[45.31px] bg-[rgba(17,17,17,0.55)] px-4 py-2">
+			{rowTexts.map((text, index) => (
+				<div key={index}>
+					<div className="flex justify-between items-center py-3 text-white">
+						<span className='font-proxima font-bold text-[15px] leading-[110%] tracking-[-2%]'>{text}</span>
+						<span className='font-proxima font-bold text-[15px] leading-[110%] tracking-[-2%]'>0.00₽</span>
+					</div>
+					{index < 4 && <div className="border-b border-[#1B2645]"></div>}
+				</div>
+			))}
+		</div>
+	)
+}
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,6 @@
-import { DepositCard } from '../components/DepositCard.tsx'
+import { DepositCard } from '../components/DepositCard.tsx';
 import { Header } from '../components/Header.tsx';
+import { IncomeStats } from '../components/ui/IncomeStats.tsx'
 
 export function Main() {
     return (
@@ -10,8 +11,17 @@ export function Main() {
                 <div className="absolute left-0 right-0 top-0 h-2 -mt-2 bg-active opacity-20 blur-sm rounded-t-[10px]"></div>
                 <div className="border-t-2 border-active rounded-t-2xl pt-6"></div>
                 <div className="mx-2">
-                   <DepositCard/>
+                    <DepositCard />
                 </div>
+                <div className="border border-[rgba(255,255,255,0.2)] backdrop-blur-[18px] bg-[linear-gradient(45deg,_rgba(255,255,255,0.06)_5.646%,_rgba(94,87,87,0.15)_94.354%)] mt-8 flex justify-center pt-[11px] pb-3 bg-opacity-50">
+                    <p className="text-[15px] font-bold font-proxima leading-[110%] tracking-[-2%] relative opacity-100">
+                        Заработано на платформе
+                    </p>
+                </div>
+              <div className='mx-2 mt-3'>
+                <IncomeStats/>
+              </div>
+             
             </div>
         </>
     );
