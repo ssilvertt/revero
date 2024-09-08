ea47c2234fa7f5ac0d9f1e8298bacde2be76b44b
silvert <silvertxd@gmail.com>
2024-09-09 01:26:09 +0200

    deploy

---
+++ vite.config.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+declare const _default: import('vite').UserConfig;
+export default _default;
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), svgr()],
+    server: {
+        port: 8080,
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@svg': fileURLToPath(new URL('./src/assets/svg', import.meta.url)),
+            '@fonts': fileURLToPath(new URL('./src/assets/fonts', import.meta.url)),
+        },
+    },
+    assetsInclude: ['**/*.otf'], // Include .otf files as assets
+    build: {
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            input: {
+                main: fileURLToPath(new URL('./index.html', import.meta.url)),
+            },
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name && assetInfo.name.endsWith('.otf')) {
+                        return 'assets/fonts/[name][extname]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
+    },
+});
